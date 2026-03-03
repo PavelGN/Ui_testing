951db7bfd91e07be1c07d9b1bc308f1cab4c82b5
PavelGN <Pavelwower@gmail.com>
2026-03-03 12:36:28 +0500

    add_options

--- conftest.py
+++ conftest.py
@@ -16,6 +16,7 @@ def driver():
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
