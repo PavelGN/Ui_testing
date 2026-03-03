fc09939dc0cededfb465d82d72e9496829f06f8a
PavelGN <Pavelwower@gmail.com>
2026-03-03 14:18:18 +0500

    fix44

--- conftest.py
+++ conftest.py
@@ -14,12 +14,13 @@ def driver():
 
     if os.getenv("CI") == "true":
         options.add_argument("--headless=new")
+        options.add_argument("--window-size=1920,1080")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--window-size=1920,1080")
+    else:
+        options.add_argument("--start-maximized")
 
     driver = webdriver.Chrome(options=options)
-    driver.maximize_window()
 
     yield driver
 
